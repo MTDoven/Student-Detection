d61138528d87260f734ee272570f83b1f778543e
MTDoven <MTDovent@gmail.com>
2024-05-29 19:50:01 +0800

    update detection training.

--- trainDetection.py
+++ trainDetection.py
@@ -7,13 +7,13 @@ if __name__ == '__main__':
     model.load("./checkpoints/origin_yolov9c.pt")
 
     model.train(data="detection_data.yaml",
+                device="cuda:0",
                 epochs=100,
                 workers=6,
-                batch=32,
+                batch=16,
                 imgsz=640,
                 save=True,
                 save_period=10,
-                cache="ram",
                 cos_lr=True,
                 close_mosaic=10,
                 resume=False,)
